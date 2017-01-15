e3a3240af9989dbe3fe1344fe3407feda8935f2b
jessi <jessica.dpr87@gmail.com>
2017-01-15 15:39:50 +0000

    Camera and gestao

---
+++ SHOME/Pages/CameraPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SHOME
+{
+	public class CameraPage : ContentPage
+	{
+		
+		public CameraPage()
+		{
+
+            Padding = new Thickness (20 , 20 , 20 , 20);
+
+            var TituloCamara = new Label
+            {
+                Text = "Nest"
+            };
+
+            var CameraNest = new WebView
+            {
+                Source = new UrlWebViewSource {
+                    Url = "https://video.nest.com/live/Ch1Vdige8J"
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    TituloCamara,
+                    CameraNest
+                }
+            };
+	
+		}
+	}
+}
+
---
+++ SHOME/Pages/LightsPage.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SHOME
+{
+	public class LightsPage : ContentPage
+	{
+
+
+        class Values
+        {
+            public Values(string titulo, string value)
+            {
+                var Titulo = titulo;
+                var Value = value;
+            }
+
+            public string Titulo { set; get; }
+
+            public string Value { set; get; }
+
+        };
+
+        public LightsPage()
+        {
+
+            Label header = new Label
+            {
+                Text = "Manage",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            // Define some data.
+            List<Values> people = new List<Values>
+            {
+                new Values("Home appliances", "65%"),
+                new Values("Otros", "35%"),
+            };
+
+            // Create the ListView.
+            ListView listView = new ListView
+            {
+                // Source of data items.
+                ItemsSource = people,
+
+                // Define template for displaying each item.
+                // (Argument of DataTemplate constructor is called for 
+                //      each item; it must return a Cell derivative.)
+                ItemTemplate = new DataTemplate(() =>
+                {
+                    // Create views with bindings for displaying each property.
+                    Label Tittle_lbl = new Label();
+                    Tittle_lbl.SetBinding(Label.TextProperty, "Tittle");
+
+                    Label Value_lbl = new Label();
+                    Value_lbl.SetBinding(Label.TextProperty, "Value");
+
+                    // Return an assembled ViewCell.
+                    return new ViewCell
+                    {
+                        View = new StackLayout
+                        {
+                            VerticalOptions = LayoutOptions.Center,
+                            Spacing = 0,
+                            Children =
+                            {
+                                Tittle_lbl,
+                                Value_lbl
+                            }
+                        }
+                    };
+                })
+
+            };
+
+        }
+
+
+    }
+}
+
