1f24d501e69976ee2fbc21b0301c01bca905bec9
filipaNobrega <filipanobrega1994@gmail.com>
2017-01-18 01:20:34 +0000

    Beacons to Android.

---
+++ Droid/BackgroundActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace SHOME.Droid
+{
+    [Activity(Label = "BackgroundActivity")]
+    public class BackgroundActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //SetContentView(Resource.Layout.ActivityBackground);
+        }
+    }
+}
