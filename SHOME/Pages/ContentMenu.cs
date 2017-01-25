033e4f7cd8c225b4e8e49e4664dedc70e33d0220
filipaNobrega <filipanobrega1994@gmail.com>
2017-01-25 14:41:03 +0000

    Content menu clean up

--- SHOME/Pages/ContentMenu.cs
+++ SHOME/Pages/ContentMenu.cs
@@ -3,15 +3,13 @@ using SHOME.Data;
 using SHOME.Pages;
 using Xamarin.Forms;
 
-
 namespace SHOME
 {
     public class ContentMenu : ContentPage
     {
-		
 		public static List<Division> Divisions { get; set; } = new List<Division>();
 		
-		public List<Division> getDivisions()
+		public List<Division> GetDivisions()
 		{
 			return Divisions;
 		}
@@ -53,14 +51,9 @@ namespace SHOME
 				devices.Add(dev);
 			}
 		}
-
-
-
-
-		//TODO se fizer sentido e for mais prático fazes aqui o foreach em vez de mandar o iterator.
+        
 		public async void DivisionData(string tab, params object[] devices)
 		{
-			
 			var aux = 0;
 			var json = await WebServicesData.SyncTask("GET", "division");
 			var size = json.Count;
@@ -68,7 +61,6 @@ namespace SHOME
 			while (size > aux)
 			{
 				var result = json[aux];
-				//TODO em vez de "idDivision" é o type (SERVER DOWN)
 				var division = new Division(
 					result["idDivision"],
 					result["divisionName"],
@@ -78,16 +70,8 @@ namespace SHOME
 				Divisions.Add(division);
 
 				DevicesData(division.Id, division, tab);
-				//var deviceLock = new Devices("lock", "lock");
-				//var deviceLights = new Devices("Luzes", "lights");
-				//var deviceCctv = new Devices("Camaras", "cctv");
-
 				aux++;
-				//TODO FOREACH para adicionar devices
 			}
-				
-
-
 		}
 
 		public async void DevicesData(int id, Division division, string tab)
@@ -151,209 +135,149 @@ namespace SHOME
 						var buttonEnergyManagement = new Image { Source = "energy_management.png", WidthRequest = 7, HeightRequest = 70 };
 						device.buttons = buttonEnergyManagement;
 						break;
-				
-
+                    default:
+				        break;
 				}
-
-
+                
 				device.buttonState = 0; 
 				division.AddDivice(device);
 				aux++;
 			}
 
-			dd(tab);
+            Construtor(tab);
 		}
-			
-		public async void dd(string tab)
+
+        private async void Construtor(string tab)
 		{
-
-		
 			var semiTransparentColor = new Color(0, 0, 0, 0.5);
-
-
+            
 			if (Device.OS == TargetPlatform.iOS)
 			{ }
 			else
-			{
-				
-
-			}
-
-			/// Device
-			//var deviceLock = new Devices("lock", "lock");
-			//var deviceLights = new Devices("Luzes", "lights");
-			//var deviceCctv = new Devices("Camaras", "cctv");
-			//var deviceAudio = new Devices("audio", "audio");
-			//var deviceBlinds = new Devices("blinds", "blinds");
-			//var deviceAC = new Devices("ac", "ac");
-			//var deviceWeather = new Devices("weather", "weather");
-			//var deviceIrrigation = new Devices("irrigation", "irrigation");
-
-
+			{ }
 
 			switch (tab)
 			{
 				case "Bedroom": BackgroundImage = "bedroom_background.png";
 					break;
-
 				case "Living Room": BackgroundImage = "lounge_background.png";
 					break;
-					
 				case "Home":BackgroundImage = "menu_background.png";
 					break;
-				
 				case "Garden":BackgroundImage = "garden_background.png";
 					break;
-			
 				case "Kitchen":BackgroundImage = "kitchen_background.png";
 					break;
-
+                default:
+			        break;
 			}
-
-		
-
-
-
-			//var buttonLights = new List<Image>();
-			//var buttonCctvs = new List<Image>();
-			//var buttonACs = new List<Image>();
-			//var buttonListAudio = new List<Image>();
-			//var buttonListBlinds = new List<Image>();
-			//var buttonListLock = new List<Image>();
-			//var buttonListIrrigation = new List<Image>();
-			//var buttonListWeather = new List<Image>();
-			//var buttonListEnergyConsumption = new List<Image>();
-			//var buttonListEnergyManagement = new List<Image>();
-
-
-
-			var stack = new StackLayout(); ////
-			Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
-
-			//var tt = await RequestData.SyncTask("division");
-
-
+            
+			var stack = new StackLayout();
+            Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+            
 			foreach (var s in Divisions)
 			{
-				if (s.Type == tab || tab == "Home")
-				{
-					var grid = new Grid 
-					{ 
-						BackgroundColor = new Color(0, 0, 0, 0),
-						RowDefinitions = new RowDefinitionCollection
-						{
-							new RowDefinition {Height = new GridLength(1, GridUnitType.Star)}
-						}
-					};
-					//grid.HorizontalOptions = LayoutOptions.StartAndExpand;
-					stack.Children.Add(new Label
-					{
-						Text = s.Name.ToUpper(),
-						HorizontalTextAlignment = TextAlignment.Center,
-						BackgroundColor = Color.FromRgba(211,211,211,100),
-						FontSize = 18,
-						FontFamily = "Roboto",
-						FontAttributes = FontAttributes.Bold
-					});
+			    if (s.Type != tab && tab != "Home") continue;
+			    var grid = new Grid 
+			    { 
+			        BackgroundColor = new Color(0, 0, 0, 0),
+			        RowDefinitions = new RowDefinitionCollection
+			        {
+			            new RowDefinition {Height = new GridLength(1, GridUnitType.Star)}
+			        }
+			    };
+			    stack.Children.Add(new Label
+			    {
+			        Text = s.Name.ToUpper(),
+			        HorizontalTextAlignment = TextAlignment.Center,
+			        BackgroundColor = Color.FromRgba(211,211,211,100),
+			        FontSize = 18,
+			        FontFamily = "Roboto",
+			        FontAttributes = FontAttributes.Bold
+			    });
 
 
-					var rowGrid = 0;
-					var columnGrid = 0;
-					foreach (var dev in s.devices)
-					{
-						grid.Children.Add(dev.buttons, columnGrid, rowGrid);
+			    var rowGrid = 0;
+			    var columnGrid = 0;
+			    foreach (var dev in s.devices)
+			    {
+			        grid.Children.Add(dev.buttons, columnGrid, rowGrid);
 
+			        if (dev.buttonState != 0) continue;
+			        switch (dev.Type)
+			        {
+			            case "Light":
+			                dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
+			                {
+			                    Command = new Command(() => { Navigation.PushModalAsync(new LightsPage()); }),
+			                    NumberOfTapsRequired = 1
+			                });
 
-						if (dev.buttonState == 0)
-						{
-							switch (dev.Type)
-							{
-								case "Light":
-									dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
-									{
-										Command = new Command(() => { Navigation.PushModalAsync(new LightsPage()); }),
-										NumberOfTapsRequired = 1
-									});
-
-									columnGrid++;
-								break;
-
-								case "CCTV":
-									dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
-									{
-										Command = new Command(() => { Navigation.PushModalAsync(new CameraPage()); }),
-										NumberOfTapsRequired = 1
-									});
-									columnGrid++;
-									break;
-
-								case "Lock":
-									dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
-									{
-										Command = new Command(() => { Navigation.PushModalAsync(new LocksPage()); }),
-										NumberOfTapsRequired = 1
-									});
-									columnGrid++;
-									break;
-
-								case "weather":
-									dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
-									{
-										Command = new Command(() => { Navigation.PushModalAsync(new Weather()); }),
-										NumberOfTapsRequired = 1
-									});
-									columnGrid++;
-									break;
-								case "EnergyConsumption":
-									dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
-									{
-										Command = new Command(() => { Navigation.PushModalAsync(new ConsumptionPage()); }),
-										NumberOfTapsRequired = 1
-									});
-									columnGrid++;
-									break;
-								case "EnergyManagement":
-									dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
-									{
-										Command = new Command(() => { Navigation.PushModalAsync(new GestaoPage()); }),
-										NumberOfTapsRequired = 1
-									});
-									columnGrid++;
-									break;
-
-								case "Events":
-									dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
-									{
-										Command = new Command(() => { Navigation.PushModalAsync(new ListEventPage()); }),
-										NumberOfTapsRequired = 1
-									});
-									columnGrid++;
-									break;
-
-							}
-
-
-							dev.buttonState = 1;
-						}
-					}
-					stack.Spacing = 20;
-
-					//grid.Opacity = 0.5;
-					stack.Children.Add(new ScrollView
-					{
-						Content = grid,
-						Orientation = ScrollOrientation.Horizontal
-					});
-				}
-
+			                columnGrid++;
+			                break;
+			            case "CCTV":
+			                dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
+			                {
+			                    Command = new Command(() => { Navigation.PushModalAsync(new CameraPage()); }),
+			                    NumberOfTapsRequired = 1
+			                });
+			                columnGrid++;
+			                break;
+			            case "Lock":
+			                dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
+			                {
+			                    Command = new Command(() => { Navigation.PushModalAsync(new LocksPage()); }),
+			                    NumberOfTapsRequired = 1
+			                });
+			                columnGrid++;
+			                break;
+			            case "weather":
+			                dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
+			                {
+			                    Command = new Command(() => { Navigation.PushModalAsync(new Weather()); }),
+			                    NumberOfTapsRequired = 1
+			                });
+			                columnGrid++;
+			                break;
+			            case "EnergyConsumption":
+			                dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
+			                {
+			                    Command = new Command(() => { Navigation.PushModalAsync(new ConsumptionPage()); }),
+			                    NumberOfTapsRequired = 1
+			                });
+			                columnGrid++;
+			                break;
+			            case "EnergyManagement":
+			                dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
+			                {
+			                    Command = new Command(() => { Navigation.PushModalAsync(new GestaoPage()); }),
+			                    NumberOfTapsRequired = 1
+			                });
+			                columnGrid++;
+			                break;
+			            case "Events":
+			                dev.buttons.GestureRecognizers.Add(new TapGestureRecognizer
+			                {
+			                    Command = new Command(() => { Navigation.PushModalAsync(new ListEventPage()); }),
+			                    NumberOfTapsRequired = 1
+			                });
+			                columnGrid++;
+			                break;
+			            default:
+			                break;
+			        }
+                        
+			        dev.buttonState = 1;
+			    }
+			    stack.Spacing = 20;
+                    
+			    stack.Children.Add(new ScrollView
+			    {
+			        Content = grid,
+			        Orientation = ScrollOrientation.Horizontal
+			    });
 			}
 			
-
-
-
-
-		
-
 			var scollVertical = new ScrollView
 			{
 				Content = stack,
@@ -362,19 +286,9 @@ namespace SHOME
 
 			Content = scollVertical;
 		}
-
-
-
-
-
+        
 		public ContentMenu(string tab)
         {
-		//	var deviceLock = new Devices("lock", "lock");
-		//	var deviceLights = new Devices("Luzes", "lights");
-
-		//	var deviceAudio = new Devices("audio", "audio");
-		//	var deviceBlinds = new Devices("blinds", "blinds");
-		//	var deviceAC = new Devices("ac", "ac");
 			var deviceWeather = new Devices(1000,"Weather", "weather");
 			var deviceEvents = new Devices(1008, "Events", "Events");
 			var deviceEnergyConsumption = new Devices(1001, "EnergyConsumption", "EnergyConsumption");
@@ -385,8 +299,7 @@ namespace SHOME
 			var buttonWeather = new Image { Source = "weather.png", WidthRequest = 7, HeightRequest = 70 };
 			var buttonEnergyConsumption = new Image { Source = "energy_consumption.png", WidthRequest = 7, HeightRequest = 70 };
 			var buttonEnergyManagement = new Image { Source = "energy_management.png", WidthRequest = 7, HeightRequest = 70 };
-
-	
+            
 		    // Division
 			var roomm = new Division(1000, "Sugest", "Home", null);
 			Divisions.Add(roomm);
@@ -401,9 +314,7 @@ namespace SHOME
 			deviceEvents.buttons = buttonEvents;
 			deviceEvents.buttonState = 0;
 			roomm.AddDivice(deviceEvents);
-
-
-
+            
 			deviceEnergyConsumption.buttons = buttonEnergyConsumption;
 			deviceEnergyConsumption.buttonState = 0;
 			roomm.AddDivice(deviceEnergyConsumption);
@@ -411,17 +322,11 @@ namespace SHOME
 			deviceEnergyManagement.buttons = buttonEnergyManagement;
 			deviceEnergyManagement.buttonState = 0;
 			roomm.AddDivice(deviceEnergyManagement);
-
-		
-
+            
 			roomm.AddDivice(deviceCctv);
 			roomm.AddDivice(deviceIrrigation);
 
 			DivisionData(tab, "sdsd");
-            
-			}
-        
-
-      
+		}
     }
 }
